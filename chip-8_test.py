bbeab5666d12e2eadc96b6dc3cdc21272b0da4aa
Dan Picton <daniel.picton@gmail.com>
2021-06-26 15:42:07 +0100

    Update tests to accommodate new Chip8 constructor signature

--- chip-8_test.py
+++ chip-8_test.py
@@ -9,17 +9,17 @@ class TestChip8(unittest.TestCase):
 
 
     def test_video_array_y(self):
-        c = Chip8((10, 12))
+        c = Chip8((10, 12), 8)
 
         self.assertEqual(len(c.video), 12)
 
     def test_video_array_x(self):
-        c = Chip8((36, 8))
+        c = Chip8((36, 8), 8)
 
         self.assertEqual(len(c.video[0]), 36)
 
     def test_fetch(self):
-        c = Chip8((1, 1))
+        c = Chip8((1, 1), 8)
         c.pc = 0
         c.memory = [255, 76, 126, 237, 1]
         c.fetch()
@@ -36,7 +36,7 @@ class TestChip8(unittest.TestCase):
     #     self.assertEqual()
 
     def test_decode_00e0(self):
-        c = Chip8((20, 20))
+        c = Chip8((20, 20), 8)
         for i in range(10):
             c.video[randint(0,18)+1][randint(0,18)+1] = 1
 
@@ -55,7 +55,7 @@ class TestChip8(unittest.TestCase):
         pass
 
     def test_decode_dXYN(self):
-        c = Chip8((10, 10))
+        c = Chip8((10, 10), 8)
         c.memory = list(hex(hexbyte) for hexbyte in [0, 0, 0, 1, 2, 4, 8, 16, 32, 64, 128, 7, 3])
         c.vregisters[3] = 0x0
         c.vregisters[5] = 0x0
