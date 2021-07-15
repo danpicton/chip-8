7c030e67b791aeb22df87495d9d623a6cf78193b
Dan Picton <danpicton@users.noreply.github.com>
2021-07-15 07:32:00 +0100

    Convert tests to use decorators for setup (#1)
    * Add non-functional test decoration code
    
    * Decorate non-video tests
    
    * Tidy imports
    
    * Create video_test decorator factory
    
    * Convert video tests to use decorator factory

--- chip-8_test.py
+++ chip-8_test.py
@@ -1,46 +1,55 @@
 import unittest
 from random import randint
+from chip8 import Chip8
 
-import chip8
-from chip8 import Chip8, Display
+# Factory for decorators
+def video_test(screen_res, pixel_size):
+    def do_test_wrapped(func):
+        def do_test(self):
+            test_chip8 = Chip8(screen_res, pixel_size)
+
+            func(self, test_chip8)
+
+            del test_chip8
+
+        return do_test
+
+    return do_test_wrapped
+
+def non_video_test(func):
+    def do_test(self):
+        test_chip8 = Chip8((1, 1), 1)
+
+        func(self, test_chip8)
+
+        del test_chip8
+
+    return do_test
+
 
 class TestChip8(unittest.TestCase):
 
-
-
-    def test_video_array_y(self):
-        c = Chip8((10, 12), 8)
+    @video_test((10, 12), 8)
+    def test_video_array_y(self, c):
 
         self.assertEqual(len(c.video), 12)
 
-        del c
-
-    def test_video_array_x(self):
-        c = Chip8((36, 8), 8)
+    @video_test((36, 8), 8)
+    def test_video_array_x(self, c):
 
         self.assertEqual(len(c.video[0]), 36)
 
-    def test_fetch(self):
-        c = Chip8((1, 1), 8)
+    @non_video_test
+    def test_fetch(self, c):
+
         c.pc = 0
         c.memory = [255, 76, 126, 237, 1]
         c.fetch()
 
         self.assertEqual(c.opcode, 331)
 
-        del c
-
-    # def test_decode(self):
-    #     c = Chip8((1, 1))
-    #     c.pc = 0
-    #     c.memory = [255, 76, 126, 237, 1]
-    #     c.fetch()
-    #     c.decode()
-    #
-    #     self.assertEqual()
-
-    def test_decode_00e0(self):
-        c = Chip8((20, 20), 8)
+    @video_test((20, 20), 8)
+    def test_decode_00e0(self, c):
 
         for i in range(10):
             c.video[randint(0,18)+1][randint(0,18)+1] = 1
@@ -50,21 +59,16 @@ class TestChip8(unittest.TestCase):
 
         self.assertEqual(sum(list(map(sum, c.video))), 0)
 
-        del c
 
-    def test_decode_1NNN(self):
-        c = Chip8((1, 1), 8)
-
+    @non_video_test
+    def test_decode_1NNN(self, c):
         c.opcode = '12ef'
         c.decode()
 
         self.assertEqual(c.pc, int('2ef', 16))
 
-        del c
-
-    def test_decode_7XNN(self):
-        c = Chip8((1, 1), 1)
-
+    @non_video_test
+    def test_decode_7XNN(self, c):
         c.vregisters[3] = 0x3f
 
         c.opcode = '730a'
@@ -72,20 +76,15 @@ class TestChip8(unittest.TestCase):
 
         self.assertEqual(c.vregisters[3], 0x3f + 0x0a)
 
-        del c
-
-    def test_decode_aNNN(self):
-        c = Chip8((1, 1), 1)
-
+    @non_video_test
+    def test_decode_aNNN(self, c):
         c.opcode = 'a1ef'
         c.decode()
 
         self.assertEqual(c.index, 0x1ef)
 
-        del c
-
-    def test_decode_dXYN(self):
-        c = Chip8((10, 10), 8)
+    @video_test((10, 10), 8)
+    def test_decode_dXYN(self, c):
         c.memory = list(hex(hexbyte) for hexbyte in [0, 0, 0, 1, 2, 4, 8, 16, 32, 64, 128, 7, 3])
         c.vregisters[3] = 0x0
         c.vregisters[5] = 0x0
@@ -105,8 +104,6 @@ class TestChip8(unittest.TestCase):
                     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
         self.assertEqual(c.video, expected)
-
-        del c
 
 if __name__ == '__main__':
     unittest.main()
