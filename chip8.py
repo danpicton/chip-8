ab38a5a9f99296ccc0d148fec055ebcab418694f
Dan Picton <daniel.picton@gmail.com>
2021-07-19 07:15:39 +0100

    Change load method to load bytes to int array

--- chip8.py
+++ chip8.py
@@ -69,14 +69,15 @@ class Chip8:
 
     def load(self, rom_path):
         with open(rom_path, "rb") as f:
-            byte = int.from_bytes(f.read(1), byteorder='big')
-            # byte = f.read(1).hex()
-            while True: # TODO: workout what this condition should be
-                self.memory[self.pc] = byte
+
+            while True:
+                byte = f.read(1)
+                if not byte: #eof
+                    break
+                self.memory[self.pc] = int.from_bytes(byte, byteorder='big')
                 # print(byte)
                 self.pc += 0x1
-                # byte = f.read(1).hex()
-                byte = int.from_bytes(f.read(1), byteorder='big')
+
         f.close()
         self.pc = MEM_ADDRESS_START
 
